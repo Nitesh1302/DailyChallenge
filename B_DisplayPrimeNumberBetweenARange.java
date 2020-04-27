be87a091ba866d3ee1f03189fe2770a222a07edc
ngoyal <ngoyal@wundermancommerce.com>
2020-04-28 00:02:21 +0530

    Print prime numbers among two given numbers

---
+++ B_DisplayPrimeNumberBetweenARange.java
@@ -0,0 +1,25 @@
+
+public class B_DisplayPrimeNumberBetweenARange {
+	
+	public static void main(String args[]) {
+	
+		displayAllPrimeNumberBetween(2,);
+	}
+
+	public static void displayAllPrimeNumberBetween(int a, int b){
+		
+		System.out.println("Prime numbers are");
+	loop1:	for(int i=a;i<b;i++) {
+				int var=0;
+		loop2:	for(int j=2;j<i;j++) {
+				if(i%j==0) {
+				var++;
+				continue loop1;
+				}
+			}
+			if(var==0) {
+				System.out.printf(i+"," );
+			}
+		}
+	}
+}
